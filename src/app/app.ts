365acafd0bf6f89bae2a3f51a3d9a223d983027c
yourhypeguy <105666787+yourhypeguy@users.noreply.github.com>
2026-04-15 15:06:20 +0100

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavBar } from './nav-bar/nav-bar';   
-import { GreetingSection } from './greeting-section/greeting-section';
-import { BestSellingContainer } from './best-selling-container/best-selling-container';
-import { FoodHomePage } from './food-home-page/food-home-page';
-import { HouseHoldHomePage } from './house-hold-home-page/house-hold-home-page';
-import { Kids } from './kids/kids';
-import { ToiletHomePage } from './toilet-home-page/toilet-home-page';
-import { RizzyDocumentation } from './rizzy-documentation/rizzy-documentation';
+import { NavBar } from './Layout/nav-bar/nav-bar';   
+import { GreetingSection } from './pages/greeting-section/greeting-section';
+import { BestSellingContainer } from './pages/best-selling-container/best-selling-container';
+import { FoodHomePage } from './pages/food-home-page/food-home-page';
+import { HouseHoldHomePage } from './pages/house-hold-home-page/house-hold-home-page';
+import { Kids } from './pages/kids/kids';
+import { ToiletHomePage } from './pages/toilet-home-page/toilet-home-page';
+import { RizzyDocumentation } from './pages/rizzy-documentation/rizzy-documentation';
+import { FooterSection } from './Layout/footer-section/footer-section';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar, GreetingSection, BestSellingContainer, FoodHomePage, HouseHoldHomePage, Kids, ToiletHomePage, RizzyDocumentation],
+  imports: [RouterOutlet, NavBar, GreetingSection, BestSellingContainer, FoodHomePage, HouseHoldHomePage, Kids, ToiletHomePage, RizzyDocumentation, FooterSection],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
