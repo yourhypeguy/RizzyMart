defb2b44b3ec2e6e52b63b8a52ef2653f265019b
yourhypeguy <105666787+yourhypeguy@users.noreply.github.com>
2026-04-23 17:12:19 +0100

    update

--- src/app/component/food-items/food-items.ts
+++ src/app/component/food-items/food-items.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,   // 
   selector: 'app-food-items',
   imports: [],
   templateUrl: './food-items.html',
--- src/app/pages/food-main-page/food-main-page.ts
+++ src/app/pages/food-main-page/food-main-page.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FoodItems } from '../../component/food-items/food-items';
+
+
 
 @Component({
   selector: 'app-food-main-page',
-  imports: [],
+  imports: [CommonModule, FoodItems, ],
   templateUrl: './food-main-page.html',
   styleUrl: './food-main-page.css',
 })
